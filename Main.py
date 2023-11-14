f58c7144c805f0fbe446ee299b437e758a041290
jingchengsimon <jingcheng.shi@rutgers.edu>
2023-11-14 11:26:19 -0500

    update on 11/14/23

--- Main.py
+++ Main.py
@@ -1,4 +1,3 @@
-
 from CellwithNetworkx import *
 from DistanceAnalyzer import *
 import matplotlib.pyplot as plt
@@ -14,9 +13,10 @@ NUM_SYN_APIC_EXC = 16070
 
 NUM_SYN_BASAL_INH = 1023
 NUM_SYN_APIC_INH = 1637
+# Is it possible to save the above part so we don't need to run the bg part every time
 
 num_syn_clustered, k, cluster_radius = 50, 5, 5
-order = 10
+order = 5
 
 # bin_array = np.array([0, 2.7, 4.5, 7.4, 12, 20, 33, 55, 90, 148, 245])
 
@@ -25,13 +25,27 @@ cell1.add_background_synapses(NUM_SYN_BASAL_EXC,
                               NUM_SYN_APIC_EXC, 
                               NUM_SYN_BASAL_INH, 
                               NUM_SYN_APIC_INH)
-cell1.add_clustered_synapses(num_syn_clustered, k, cluster_radius, order) # 45 degree
+cell1.add_clustered_synapses(num_syn_clustered, k, cluster_radius, order) 
+cell1.visualize_tuning_curve()
+
+# 创建保存文件夹
+pref_ori_dg = 0
+folder_path = f'./results/simulation/spt/dist{order}_degree{pref_ori_dg}_size{int(num_syn_clustered/k)}'
+if not os.path.exists(folder_path):
+    os.makedirs(folder_path)
+    
+# 保存所有打开的图形到指定文件夹
+for i in plt.get_fignums():
+    plt.figure(i)
+    file_path = os.path.join(folder_path, f'figure_{i}.png')
+    plt.savefig(file_path)
+    plt.close(i)
 
 # # tuning curve
 # input: 0 45 90.. -> cluster
 # cell1.visualize_synapses('Background + Clustered Synapses')
 
-plt.show()
+# plt.show()
 
 # type_array = cell1.add_clustered_synapses(num_synapses_to_add,k,cluster_radius)
 
