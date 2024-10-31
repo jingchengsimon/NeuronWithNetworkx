c0911fb9de960415ea30cb824098bf915fa1e67a
jingchengsimon <js3269@scarletmail.rutgers.edu>
2024-10-31 11:28:13 -0400

    update on ubuntu 10/31/24

---
+++ .history/codesForTest/graph_bifurcation_20241031110728.py
@@ -0,0 +1,100 @@
+import networkx as nx
+import matplotlib.pyplot as plt
+from networkx.drawing.nx_pydot import graphviz_layout
+
+def plot_graph(G):
+    plt.figure()
+    # pos = nx.spring_layout(G)
+    # nx.draw(G, pos, with_labels=True, node_size=700, node_color='skyblue')
+
+    # Define colors based on node indices
+    node_colors = []
+    for node in G.nodes:
+        if node == 0:
+            node_colors.append('lightgreen')
+        elif 1 <= node < 5:
+            node_colors.append('skyblue')
+        elif 5 <= node <= 7:
+            node_colors.append('lightgray')
+        elif node > 7:
+            node_colors.append('lightpurple')
+        else:
+            node_colors.append('lightgray')  # Default color if any nodes fall outside expected ranges
+
+    pos = graphviz_layout(G, prog="dot")
+    nx.draw(G, pos, node_size=70, node_color=node_colors)
+
+    nx.draw_networkx_labels(G, pos, labels={node: node for node in G.nodes}, font_size=8, font_color="black")
+    
+    # 标记节点的 order
+    # nx.draw_networkx_labels(G, pos, labels=order, font_size=12, font_color='red')
+    
+    plt.title("Graph Visualization")
+    # plt.show()
+
+def get_subgraph(G, node):
+    subgraph_nodes = nx.descendants(G, node) | {node}  # 使用descendants函数获取所有后代节点
+    subgraph = G.subgraph(subgraph_nodes)  # 创建一个包含指定节点及其后代节点的子图
+
+    # 打印新图的节点
+    print("Nodes in subgraph:", subgraph.nodes())
+
+    return subgraph
+
+def out_degree(G,root_node=0):
+    
+    # 统计每个节点的出度
+    out_degree = dict(G.out_degree())
+
+    print("每个节点的出度:")
+    print(out_degree)
+
+    # 计算每个节点到起点的最短路径
+    shortest_paths = nx.single_source_shortest_path_length(G, source=root_node)
+    # print(shortest_paths)
+
+    # 将除起点以外出度大于1的节点设为分岔点
+    fork_points = {node for node, degree in out_degree.items() if node != 0 and degree > 1}
+
+    # class_dict = {}
+    # for node, order in shortest_paths.items():
+    #     if order not in class_dict:
+    #         class_dict[order] = []
+    #     class_dict[order].append(node)
+
+    # # 输出分类结果
+    #     max_order = max(shortest_paths.values())
+
+    # for i in range(max_order + 1):
+    #         print(f"Class {i}: {class_dict.get(i, [])}")
+
+
+    # 统计每个节点的最短路径中分岔点的个数，并设为 order
+    class_dict = {}
+    for node, distance in shortest_paths.items():
+        forks_in_path = sum(1 for n in nx.shortest_path(G, source=root_node)[node][1:-1] if n in fork_points)
+        order = forks_in_path
+        if order not in class_dict:
+            class_dict[order] = []
+        class_dict[order].append(node)
+
+    max_order = max(class_dict)
+    for i in range(max_order + 1):
+        print(f"Class {i}: {class_dict.get(i, [])}")
+
+# 创建一个示例图
+G = nx.DiGraph()
+G.add_edges_from([(0, 1), (1, 2), (2, 3), (3, 4), (4, 5), (2, 6), (6, 7), (4, 8)])
+
+# 选择节点1及其所有子节点，创建一个新图
+sub_G = get_subgraph(G, 2)
+out_degree(G, 0)
+out_degree(sub_G, 2)
+plot_graph(G)
+# plot_graph(sub_G)
+plt.show()
+
+
+# print("每个节点的 order 标记:")
+# print(order)
+
---
+++ .history/utils/generate_stim_utils_20241025142158.py
@@ -0,0 +1,68 @@
+import glob
+import pandas as pd
+import numpy as np
+from neuron import h
+
+from neuron.units import mV
+import matplotlib.pyplot as plt
+import os
+
+def generate_indices(rnd, num_clusters, num_conn_per_preunit, num_preunit):
+    
+    pref_ori_dg = 0
+    unit_ids = np.arange(num_preunit)
+    
+    results = [] 
+    indices = []
+    for _ in range(len(unit_ids)):
+        # choose num_conn clusters without replacement
+        sampled = rnd.choice(num_clusters, num_conn_per_preunit, replace=False)  
+        results.append(sampled)
+
+    for i in range(num_clusters):
+        index_list = [j for j, lst in enumerate(results) if i in lst]
+        indices.append(index_list)
+
+    return pref_ori_dg, unit_ids, indices
+
+def generate_vecstim(unit_ids, num_stim, stim_time, folder_path):
+    spt_unit_list = []
+    
+    for _ in unit_ids:
+        ## Artificial spike trains
+        # try:
+        #     spt_unit = spt_grouped_df.get_group((unit_id, stim_id))
+        #     spt_unit = (spt_unit['spike_time'].values - spt_unit['spike_time'].values[0]) * 1000
+        # except KeyError:
+        #     # for units not fired, add list of 0
+        #     spt_unit = np.array([])
+
+        ## Single/Double Netstim
+        
+        # netstim = h.NetStim()
+        # netstim.number = num_stim
+        # netstim.interval = 3 # ms (the default value is actually 10 ms)
+        # netstim.start = 500 #np.random.normal(500, 5)
+        # netstim.noise = 0
+        # spt_unit = netstim
+
+        spt_unit = np.floor(np.random.normal(stim_time, 5, num_stim))
+        spt_unit_list.append(spt_unit)
+     
+    return spt_unit_list
+
+def get_stim_ids(ori_dg):
+    spt_path = 'C:/Users/Windows/Desktop/MIMOlab/Codes/AllenAtlas/results/spt_df'
+    pref_ori_dg = 0
+    session_id = 732592105
+
+    # for calculate the OSI
+    spt_file = glob.glob(spt_path + f'/{pref_ori_dg}_{session_id}/spt_{ori_dg}.csv')
+    file_path = spt_file[0] # usually only one file
+    spt_df = pd.read_csv(file_path, index_col=None, header=0)
+    
+    # we need the presynaptic units always the same
+    stim_ids = np.sort(spt_df['stimulus_presentation_id'].unique())
+    print(f'stim_ids: {stim_ids}')
+    
+    return stim_ids
